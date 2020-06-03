bccfe4d53e88e1032c277b494fe6c2c6c8d00c02
Oliver Voeltz <frechmatz@gmx.de>
2020-06-03 19:05:31 +0200

    Windows ABCL

--- script/abcl-run-tests.bat
+++ script/abcl-run-tests.bat
@@ -1,3 +1,3 @@
 REM Run tests with abcl
 REM java -jar c:\abcl\abcl-bin-1.4.0\abcl.jar --batch --load run-tests.lisp 
-abcl --batch --load run-tests.lisp 
+abcl.bat --batch --load run-tests.lisp 
---
+++ script/abcl.bat
@@ -0,0 +1,2 @@
+
+java -jar c:\Users\ovoel\abcl\abcl-bin-1.6.1\abcl.jar %*
