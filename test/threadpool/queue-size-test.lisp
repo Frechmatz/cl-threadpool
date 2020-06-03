60c26c734cef7f113d34dcebad8cfdf72c4c4d2c
Oliver Voeltz <frechmatz@gmx.de>
2020-06-03 21:01:16 +0200

    Compiler warnings

--- test/threadpool/queue-size-test.lisp
+++ test/threadpool/queue-size-test.lisp
@@ -3,14 +3,13 @@
 (define-test queue-size-test-1 ()
   (let ((pool (cl-threadpool:make-threadpool 1 :name "queue-size-test-1")))
     (cl-threadpool:start pool)
-    (let ((results
-	   (cl-threadpool:run-jobs
-	    pool
-	    (list
-	     (lambda() "Job 1")
-	     (lambda() "Job 2")
-	     (lambda() "Job 3")))))
-      (let ((l (cl-threadpool:queue-size pool)))
-	(cl-threadpool:stop pool)
-	(assert-equal 0 l)))))
+    (cl-threadpool:run-jobs
+     pool
+     (list
+      (lambda() "Job 1")
+      (lambda() "Job 2")
+      (lambda() "Job 3")))
+    (let ((l (cl-threadpool:queue-size pool)))
+      (cl-threadpool:stop pool)
+      (assert-equal 0 l))))
 
