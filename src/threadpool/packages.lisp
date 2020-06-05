d94c5956bec6ad194e6504659bf8f09ab383cd37
Oliver Voeltz <frechmatz@gmx.de>
2020-06-05 18:44:00 +0200

    Added pool-name to exports

--- src/threadpool/packages.lisp
+++ src/threadpool/packages.lisp
@@ -3,12 +3,12 @@
   (:export :make-threadpool)
   (:export :start)
   (:export :stop)
-;;  (:export :add-job)
   (:export :threadpoolp)
   (:export :worker-thread-p)
   (:export :threadpool-error)
   (:export :run-jobs)
   (:export :queue-size)
+  (:export :pool-name)
   (:export :*logger*))
 
 
