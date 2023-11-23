b7c6f4afeefa11fc21f933508f89c1f3c9669a25
Oliver Voeltz <frechmatz@gmx.de>
2023-11-23 19:55:56 +0100

    Indentation

--- make-doc/make-doc.lisp
+++ make-doc/make-doc.lisp
@@ -80,86 +80,108 @@
   `("<html>"
     "<head><link href=\"styles.css\" rel=\"stylesheet\" type=\"text/css\"/></head>"
     "<body>"
-    (semantic (:name "header")
-	      (heading (:name "cl-threadpool"))
-	      ,(cl-html-readme:read-file "make-doc/introduction.html")
-	      "<p>The source code is available <a href=\"https://github.com/Frechmatz/cl-threadpool\">here</a>.</p>")
-    (semantic (:name "nav")
-	      (heading (:name "Table of contents")
-		       (toc)))
-    (semantic (:name "section")
-	      (heading (:name "Examples" :toc t)
-		       (heading (:name ,(make-package-string doc-index "cl-threadpool-example-1"))
-				,(make-code-string "examples/example-1.lisp"))
-		       (heading (:name ,(make-package-string doc-index "cl-threadpool-example-2"))
-				,(make-code-string "examples/example-2.lisp")))
-	      (heading (:name "Change-Log" :toc t)
-		       (heading (:name "Version 1.0.0")
-				"<p>Initial release of cl-threadpool.</p>")
-		       (heading (:name "Version 2.0.0")
-				"<p>Version 2 is a major rework of the thread pool with bugfixes, new features and removal of features that have been identified as not being useful.</p>"
-				(heading (:name "Breaking changes")
-					 "<ul>"
-					 "<li>Removed add-job. Has been replaced with run-jobs.</li>"
-					 "<li>Removed :resignal-job-conditions argument from make-threadpool. The pool no longer handles conditions signalled by a job.</li>"
-					 "<li>Removed :max-queue-size argument from make-threadpool. The size of the job queue is now unlimited.</li>"
-					 " <li>Removed condition threadpool-error-queue-capacity-exceeded.</li>"
-					 "<li>Removed dependency 'verbose'. The library no longer depends on a logging framework.</li>"
-					 "</ul>")
-				(heading (:name "New features")
-					 "<ul>"
-					 "<li>Added run-jobs for synchronous execution of jobs.</li>"
-					 "<li>Added queue-size to get the number of jobs waiting for execution.</li>"
-					 "<li>Added pool-name to get the name of a thread pool.</li>"
-					 "</ul>"))
-		       (heading (:name "Version 3.0.0")
-				"<p>This version introduces futures and shall also be the last version coming with breaking changes.</p>"
-				(heading (:name "Breaking changes")
-					 "<ul>"
-					 "<li>Removed cl-threadpool:start. A threadpool is now ready to use immediately after its instantiation.</li>"
-					 "<li>Removed condition threadpool-error. Pool usage errors and internal errors are now	represented by simple-error.</li>"
-					 "<li>cl-threadpool:stop no longer returns an indicator if the pool has successfully been stopped. The status can be checked via cl-threadpool:pool-stopped-p.</li>"
-					 "<li>Error handling of jobs has been reworked. The threadpool now catches all unhandled conditions of jobs. If a job has signalled a condition and its result is requested, a job-execution-error is signalled by the threadpool.</li>"
-					 "</ul>")
-				(heading (:name "New features")
-					 "<ul>"
-					 "<li>Added add-job Adds a job to the queue and returns a future. The future represents the result of the asynchronously running job.</li>"
-					 "<li>Added job-result, cancel-job, job-done-p, job-cancelled-p.</li>"
-					 "<li>Added conditions job-execution-error and job-cancellation-error.</li>"
-					 "<li>Added pool-stopped-p.</li>"
-					 "</ul>"))
-		       (heading (:name "Version 3.0.1")
-				"<p><b>This version is the current quicklisp release.</b></p>"
-				"<p>Adapted to change in verbose library.</p>"))
-	      (heading (:name "Installation" :toc t)
-		       ,(cl-html-readme:read-file "make-doc/installation.html"))
-	      (heading (:name "API" :toc t)
-		       ,(make-function-string index "cl-threadpool" "make-threadpool")
-		       ,(make-function-string index "cl-threadpool" "run-jobs")
-		       ,(make-function-string index "cl-threadpool" "add-job")
-		       ,(make-function-string index "cl-threadpool" "stop")
-		       ,(make-function-string index "cl-threadpool" "threadpoolp")
-		       ,(make-function-string index "cl-threadpool" "queue-size")
-		       ,(make-function-string index "cl-threadpool" "pool-name")
-		       ,(make-function-string index "cl-threadpool" "pool-stopped-p")
-		       ,(make-function-string index "cl-threadpool" "worker-thread-p")
-		       ,(make-function-string index "cl-threadpool" "job-result")
-		       ,(make-function-string index "cl-threadpool" "job-done-p")
-		       ,(make-function-string index "cl-threadpool" "cancel-job")
-		       ,(make-function-string index "cl-threadpool" "job-cancelled-p")
-		       ,(make-condition-string index "cl-threadpool" "job-cancellation-error")
-		       ,(make-condition-string index "cl-threadpool" "job-execution-error")
-		       ,(make-function-string index "cl-threadpool" "job-execution-error-pool-name")
-		       ,(make-function-string index "cl-threadpool" "job-execution-error-message")
-		       ,(make-variable-string index "cl-threadpool" "*logger*"))
-	      (heading (:name "Run tests" :toc t)
-		       "<pre><code>(asdf:test-system :cl-threadpool)</code></pre>"
-		       (heading (:name "Tested Lisp implementations and operating systems")
-				,(cl-html-readme:read-file "make-doc/supported.html")))
-	      (heading (:name "Generate documentation" :toc t)
-		       ,(make-code-string "make-doc/generate-doc.lisp")))
-    (semantic (:name "footer")
-	      "<hr/><p><small>Generated " ,(now) "</small></p>")
+    (semantic
+     (:name "header")
+     (heading
+      (:name "cl-threadpool"))
+     ,(cl-html-readme:read-file "make-doc/introduction.html")
+     "<p>The source code is available <a href=\"https://github.com/Frechmatz/cl-threadpool\">here</a>.</p>")
+    (semantic
+     (:name "nav")
+     (heading
+      (:name "Table of contents")
+      (toc)))
+    (semantic
+     (:name "section")
+     (heading
+      (:name "Examples" :toc t)
+      (heading
+       (:name ,(make-package-string doc-index "cl-threadpool-example-1"))
+       ,(make-code-string "examples/example-1.lisp"))
+      (heading
+       (:name ,(make-package-string doc-index "cl-threadpool-example-2"))
+       ,(make-code-string "examples/example-2.lisp")))
+     (heading
+      (:name "Change-Log" :toc t)
+      (heading (:name "Version 1.0.0")
+	       "<p>Initial release of cl-threadpool.</p>")
+      (heading
+       (:name "Version 2.0.0")
+       "<p>Version 2 is a major rework of the thread pool with bugfixes, new features and removal of features that have been identified as not being useful.</p>"
+       (heading
+	(:name "Breaking changes")
+	"<ul>"
+	"<li>Removed add-job. Has been replaced with run-jobs.</li>"
+	"<li>Removed :resignal-job-conditions argument from make-threadpool. The pool no longer handles conditions signalled by a job.</li>"
+	"<li>Removed :max-queue-size argument from make-threadpool. The size of the job queue is now unlimited.</li>"
+	" <li>Removed condition threadpool-error-queue-capacity-exceeded.</li>"
+	"<li>Removed dependency 'verbose'. The library no longer depends on a logging framework.</li>"
+	"</ul>")
+       (heading
+	(:name "New features")
+	"<ul>"
+	"<li>Added run-jobs for synchronous execution of jobs.</li>"
+	"<li>Added queue-size to get the number of jobs waiting for execution.</li>"
+	"<li>Added pool-name to get the name of a thread pool.</li>"
+	"</ul>"))
+      (heading
+       (:name "Version 3.0.0")
+       "<p>This version introduces futures and shall also be the last version coming with breaking changes.</p>"
+       (heading
+	(:name "Breaking changes")
+	"<ul>"
+	"<li>Removed cl-threadpool:start. A threadpool is now ready to use immediately after its instantiation.</li>"
+	"<li>Removed condition threadpool-error. Pool usage errors and internal errors are now	represented by simple-error.</li>"
+	"<li>cl-threadpool:stop no longer returns an indicator if the pool has successfully been stopped. The status can be checked via cl-threadpool:pool-stopped-p.</li>"
+	"<li>Error handling of jobs has been reworked. The threadpool now catches all unhandled conditions of jobs. If a job has signalled a condition and its result is requested, a job-execution-error is signalled by the threadpool.</li>"
+	"</ul>")
+       (heading
+	(:name "New features")
+	"<ul>"
+	"<li>Added add-job Adds a job to the queue and returns a future. The future represents the result of the asynchronously running job.</li>"
+	"<li>Added job-result, cancel-job, job-done-p, job-cancelled-p.</li>"
+	"<li>Added conditions job-execution-error and job-cancellation-error.</li>"
+	"<li>Added pool-stopped-p.</li>"
+	"</ul>"))
+      (heading
+       (:name "Version 3.0.1")
+       "<p><b>This version is the current quicklisp release.</b></p>"
+       "<p>Adapted to change in verbose library.</p>"))
+     (heading
+      (:name "Installation" :toc t)
+      ,(cl-html-readme:read-file "make-doc/installation.html"))
+     (heading
+      (:name "API" :toc t)
+      ,(make-function-string index "cl-threadpool" "make-threadpool")
+      ,(make-function-string index "cl-threadpool" "run-jobs")
+      ,(make-function-string index "cl-threadpool" "add-job")
+      ,(make-function-string index "cl-threadpool" "stop")
+      ,(make-function-string index "cl-threadpool" "threadpoolp")
+      ,(make-function-string index "cl-threadpool" "queue-size")
+      ,(make-function-string index "cl-threadpool" "pool-name")
+      ,(make-function-string index "cl-threadpool" "pool-stopped-p")
+      ,(make-function-string index "cl-threadpool" "worker-thread-p")
+      ,(make-function-string index "cl-threadpool" "job-result")
+      ,(make-function-string index "cl-threadpool" "job-done-p")
+      ,(make-function-string index "cl-threadpool" "cancel-job")
+      ,(make-function-string index "cl-threadpool" "job-cancelled-p")
+      ,(make-condition-string index "cl-threadpool" "job-cancellation-error")
+      ,(make-condition-string index "cl-threadpool" "job-execution-error")
+      ,(make-function-string index "cl-threadpool" "job-execution-error-pool-name")
+      ,(make-function-string index "cl-threadpool" "job-execution-error-message")
+      ,(make-variable-string index "cl-threadpool" "*logger*"))
+     (heading
+      (:name "Run tests" :toc t)
+      "<pre><code>(asdf:test-system :cl-threadpool)</code></pre>"
+      (heading
+       (:name "Tested Lisp implementations and operating systems")
+       ,(cl-html-readme:read-file "make-doc/supported.html")))
+     (heading
+      (:name "Generate documentation" :toc t)
+      ,(make-code-string "make-doc/generate-doc.lisp")))
+    (semantic
+     (:name "footer")
+     "<hr/><p><small>Generated " ,(now) "</small></p>")
     "</body></html>"))
 
 ;;
@@ -171,11 +193,12 @@
 	(cl-html-readme:*tab-width* 4)
 	(index (make-index :cl-threadpool))
 	(doc-index (make-index :cl-threadpool/doc)))
-    (with-open-file (fh (cl-html-readme:make-path "docs/index.html")
-			:direction :output
-			:if-exists :supersede
-			:if-does-not-exist :create
-			:external-format :utf-8)
+    (with-open-file
+	(fh (cl-html-readme:make-path "docs/index.html")
+	    :direction :output
+	    :if-exists :supersede
+	    :if-does-not-exist :create
+	    :external-format :utf-8)
       (cl-html-readme:doc-to-html fh (get-readme index doc-index))))
   "DONE")
 
